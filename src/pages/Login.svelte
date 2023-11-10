a047e7a5b8789cfeefcfb69a8e1345851db1d876
Valdir Amaral <valdir.amaral58@gmail.com>
2023-11-09 21:32:29 -0300

    feat: create account page

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -3,34 +3,70 @@
     import {pbStore} from '../stores';
     import { push, replace } from 'svelte-spa-router';
 
-    let name, password;
+    let name, password, warn;
     
+    let state = 'login';
+
+    let data = {}
+    function changeState() {
+        state = 'sign-in'
+    }
     onMount(() => {
         /*if (localStorage.user) {
             replace('/users')
         }
         */
     })
-    const login = () => {
+    const login = (name, password) => {
         $pbStore.collection('users').authWithPassword(name, password)
         .then(res => {
             localStorage.setItem('user', $pbStore.authStore.model.id)
             push('/users')
         })
     }
+
+    const signIn = () => {
+        console.log(data)
+        $pbStore.collection('users').create(data)
+        .then(res => login(data.username, data.password))
+    }
+
+    const checkPasswords = () => {
+        if (data.password && data.passwordConfirm && data.password != data.passwordConfirm) {
+            warn = "Senhas não correspondem";
+        } else {
+            warn = ''
+        }
+    }
 </script>
 
 <div class="container">
+    {#if state == 'login'}
     <h1>Login</h1>
     <p>Entre agora na sua conta</p>
 
     <div class="form">
-        <input placeholder="E-mail" bind:value={name} type="text">
+        <input placeholder="Nome de usuário ou e-mail" bind:value={name} type="text">
         <input placeholder="Senha" bind:value={password} type="text">
     </div>
 
-    <button class="sign-in" on:click={login}>Login</button>
+    <p>Não tem uma conta? <a on:click={changeState}>Crie uma agora!</a></p>
+    <button class="sign-in" on:click={() => login(name, password)}>Login</button>
 
+    {:else if state == 'sign-in'}
+    <h1>Criar conta</h1>
+    <p>Crie agora uma conta, é fácil e rápido :)</p>
+
+    <div class="form">
+        <input type="text" bind:value={data.name} placeholder="Seu nome*">
+        <input bind:value={data.username} type="text" placeholder="Nome de usuário*">
+        <input bind:value={data.email} type="email" placeholder="E-mail">
+        <input on:blur={checkPasswords} on:keypress={checkPasswords} bind:value={data.password} type="password" placeholder="Senha*">
+        <input on:blur={checkPasswords} on:keypress={checkPasswords} bind:value={data.passwordConfirm} type="password" placeholder="Confirmar senha*">
+    </div>
+    {#if warn}<p>{warn}</p>{/if}
+    <button class="sign-in" on:click={signIn}>Criar conta</button>
+    {/if}
 </div>
 
 
@@ -54,9 +90,8 @@ p {
     background-color: transparent;
     border: none;
     border-bottom: 1px solid gray;
-    padding: 15px 10px;
+    padding: 15px 25px;
     color: white;
-    border-radius: 10px;
 }
 .form input:focus {
     outline: none;
