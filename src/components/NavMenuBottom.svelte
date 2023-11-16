9e067055f7d7988da71d340c5d4b6f794ea4bb4a
Valdir Amaral <valdir.amaral58@gmail.com>
2023-11-15 23:10:47 -0300

    style: add icons for nav links

--- src/components/NavMenuBottom.svelte
+++ src/components/NavMenuBottom.svelte
@@ -7,13 +7,28 @@
 <nav>
     <footer>
         <div>
-            <a use:active={'/explorar'} use:link href="/explorar">Explorar</a>
+            <a use:active={'/explorar'} use:link href="/explorar">
+                <span class="material-icons">
+                explore
+                </span>
+                Explorar
+            </a>
         </div>
         <div>
-            <a use:active={'/chat'} use:link href="/chat">Mensagens</a>
+            <a use:active={'/chat'} use:link href="/chat">
+                <span class="material-icons">
+                chat_bubble
+                </span>
+                Mensagens
+            </a>
         </div>
         <div>
-            <a use:active={'/perfil'} use:link href="/perfil">Perfil</a>
+            <a use:active={'/perfil'} use:link href="/perfil">
+                <span class="material-icons">
+                account_circle
+                </span>
+                Perfil
+            </a>
         </div>
     </footer>
 </nav>
@@ -22,7 +37,7 @@
 <style>
     nav {
         background-color: #000000;
-        padding: 20px;
+        padding: 10px 20px 20px 20px;
         position: absolute;
         bottom: 0;
         width: 100%;
@@ -34,11 +49,19 @@
         position: relative;
     }
     nav footer a {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
         font-size: 13px;
         color: #5e5e5e;
+        gap: 8px;
         padding: 5px 5px 0 5px;
         text-decoration: none;
         transition: all .3s ease-in-out;
+    }
+    nav footer a span {
+        text-align: center;
+        font-size: 16px;
     }
     nav footer > div {
         position: relative;
@@ -51,7 +74,7 @@
         height: 10px;
         background-color: #0e1013;
         position: absolute;
-        top: -20px;
+        top: -10px;
         left: 0;
         width: 100%;
         border-radius: 0 0 10px 10px;
