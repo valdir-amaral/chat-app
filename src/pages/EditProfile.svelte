1a3ef84538562d7a92fea101d2d328803659b713
Valdir Amaral <valdir.amaral58@gmail.com>
2023-11-16 15:35:57 -0300

    style: edit profile input and avatar input

--- src/pages/EditProfile.svelte
+++ src/pages/EditProfile.svelte
@@ -4,7 +4,7 @@
     import { pbStore } from '../stores';
     let fileInput, fileName = "";
 
-    let newBio, newDescription;
+    let newBio, newDescription, loading = false;
     const formData = new FormData();
 
     const changeInputFile = ev => {
@@ -14,11 +14,14 @@
     }
     
     const saveChanges = () => {
-
+        loading = true;
         if (newBio) formData.append('bio', newBio)
         if (newDescription) formData.append('description', newDescription)
         $pbStore.collection('users').update(localStorage.user, formData)
-        .then(res => console.log(res))
+        .then(res => {
+            loading = false;
+            push('/perfil');
+        })
     }
 </script>
 
@@ -30,14 +33,18 @@
 </div>
 
 <div class="container">
-    <label for="avatar-input">
-        Trocar foto {fileName}
+    <label class="file-input" for="avatar-input">
+        Foto de perfil
+        <span class="material-icons">
+            upload
+        </span>
+        Selecione um arquivo {fileName}
     </label>
 
     <input type="file" on:change={changeInputFile} bind:files={fileInput} name="avatar-input" id="avatar-input">
-    <input bind:value={newBio} type="text" placeholder="Bio"> -> campo feito na maior preguiça
-    <input bind:value={newDescription} type="text" placeholder="Descrição"> -> campo feito na maior preguiça
-    <a on:click={saveChanges}>Salvar alterações</a>
+    <input bind:value={newBio} type="text" placeholder="Bio"> 
+    <input bind:value={newDescription} type="text" placeholder="Descrição">
+    <button disabled={!newBio && !newDescription && loading && fileInput} class="sign-in" on:click={saveChanges}>{#if loading}<span class="loading-white"></span>{:else}Salvar alterações{/if}</button>
 </div>
 
 <style>
@@ -47,18 +54,42 @@
         gap: 8px;
         align-items: center;
     }
+    
     input {
-        display: block;
-        margin-top: 20px;
+    transition: all .2s ease-in-out;
+    transition: border-radius .3s ease-in-out;
+    background-color: transparent;
+    margin-bottom: 20px;
+    border: 1px solid #494949;
+    border-radius: 20px;
+    padding: 12px 25px;
+    color: white;
+    width: 100%;
+}
+    .file-input {
+        border-radius: 20px;
+        border: 1px solid #494949;
+        padding: 30px;
+        color: #757575;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 8px;
+        margin-bottom: 20px;
     }
     label {
         display: block;
         margin-top: 50px;
+        text-align: center;
     }
     #avatar-input {
         display: none;
     }
-    .container a {
+    .container button {
+        width: 100%;
+        color: white;
+        border: none;
         margin-top: 50px;
         display: block;
         background-color: purple;
